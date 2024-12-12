3e4451702f7890cefc6851561001c68efbbde503
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-12 11:50:38 +0000

    Discoteca.kt: Creamos la dataclass de Discoteca

--- app/src/main/java/com/example/finalprojectprogramacion/data/Discoteca.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/data/Discoteca.kt
@@ -1,4 +1,18 @@
-package com.example.finalprojectprogramacion.data
+package com.example.finalprojectprogramacion
 
-class Discoteca {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
+
+data class Discoteca(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val latitude: String,
+    val longitude: String,
+    val IdTipoDiscoteca: Int,
+    val description: String,
+    val horario: String,
+    val capacidad: String,
+    val telefono: String
+)
