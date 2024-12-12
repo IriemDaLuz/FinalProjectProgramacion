faa555cf5edf6bd0ab82a7172cdac82e97c7c6c5
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-12 11:54:51 +0000

    Reserva.kt: Creamos la dataclass de Reserva

--- app/src/main/java/com/example/finalprojectprogramacion/data/Reserva.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/data/Reserva.kt
@@ -1,4 +1,17 @@
-package com.example.finalprojectprogramacion.data
+package com.example.finalprojectprogramacion
 
-class Reserva {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
+
+
+data class Reserva(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val fechaReserva: String,
+    val fechaEvento: String,
+    val idDiscoteca: Int,
+    val cantidadPersonas: Int? = null,
+    val estado: String
+
+)
