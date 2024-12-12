a78d477ca26a7074ba4b5503e2e0496633337f1e
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-12 11:58:38 +0000

    TipoDiscotecaDao.kt: Logica de dao de TipoDiscoteca

---
+++ app/src/main/java/com/example/finalprojectprogramacion/data/TipoDiscotecaDao.kt
@@ -0,0 +1,14 @@
+package com.example.finalprojectprogramacion.data
+
+import androidx.room.*
+
+@Dao interface TipoDiscotecaDao {
+    @Insert
+    suspend fun insertMarkerType(markerType: TipoDiscotecaDao)
+
+    @Query("SELECT * FROM tiposdiscotecas")
+    suspend fun getAllMarkerTypes(): List<TipoDiscotecaDao>
+
+    @Query("SELECT * FROM tiposdiscotecas WHERE id = :id")
+    fun getMarkTypeById(id: Int): TipoDiscotecaDao?
+}
