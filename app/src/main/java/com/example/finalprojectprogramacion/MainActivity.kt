19d315a6380b3c84e472ee28f6708ee4c54b34d0
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-12 12:06:08 +0000

    MainActivity.kt: Creamos variables para acceder a la base de datos

--- app/src/main/java/com/example/finalprojectprogramacion/MainActivity.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/MainActivity.kt
@@ -1,47 +1,24 @@
 package com.example.finalprojectprogramacion
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.finalprojectprogramacion.ui.theme.FinalProjectProgramacionTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val database = BaseDeDatos.getDatabase(this)
+        val DiscotecaDao = database.DiscotecaDao()
+        val TipoDiscotecaDao = database.TipoDiscotecaDao()
+        val ReservaDao = database.ReservaDao()
+
+        val ViewModelDiscotecaApp = ViewModelApp(DiscotecaDao, TipoDiscotecaDao,ReservaDao)
+
         setContent {
-            FinalProjectProgramacionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainApp(ViewModelDiscotecaApp)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FinalProjectProgramacionTheme {
-        Greeting("Android")
     }
 }
