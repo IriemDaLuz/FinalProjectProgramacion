e41e56faf09857b8135f8e1054aa0883d19f58ab
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-12 11:55:38 +0000

    TipoDiscoteca.kt: Creamos la tabla y dataclass de TipoDiscoteca

--- app/src/main/java/com/example/finalprojectprogramacion/data/TipoDiscoteca.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/data/TipoDiscoteca.kt
@@ -1,4 +1,11 @@
-package com.example.finalprojectprogramacion.data
+package com.example.finalprojectprogramacion
 
-class TipoDiscoteca {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tiposdiscotecas")
+data class TipoDiscoteca(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String
+)
