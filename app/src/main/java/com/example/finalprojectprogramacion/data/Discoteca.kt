578112d27873fb8dc25f4fbd6d24f9070a11d2c6
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-12 11:51:00 +0000

    Discoteca.kt: Creamos la tabla de Discoteca

--- app/src/main/java/com/example/finalprojectprogramacion/data/Discoteca.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/data/Discoteca.kt
@@ -4,6 +4,17 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.ForeignKey
 
+@Entity(
+    tableName = "discotecas",
+    foreignKeys = [
+        ForeignKey(
+            entity = TipoDiscoteca::class,
+            parentColumns = ["id"],
+            childColumns = ["IdTipoDiscoteca"],
+        )
+    ]
+)
+
 data class Discoteca(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
