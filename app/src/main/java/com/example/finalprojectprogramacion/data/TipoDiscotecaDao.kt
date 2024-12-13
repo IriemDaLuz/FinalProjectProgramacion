cc1b2848824c29b31d96a6816638cf081b167c77
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-13 11:23:48 +0000

    TipoDiscotecaDao.kt: Terminamos TipoDiscotecaDao.kt

--- app/src/main/java/com/example/finalprojectprogramacion/data/TipoDiscotecaDao.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/data/TipoDiscotecaDao.kt
@@ -4,11 +4,11 @@ import androidx.room.*
 
 @Dao interface TipoDiscotecaDao {
     @Insert
-    suspend fun insertMarkerType(markerType: TipoDiscotecaDao)
+    suspend fun insertMarkerType(markerType: TipoDiscoteca)
 
     @Query("SELECT * FROM tiposdiscotecas")
-    suspend fun getAllMarkerTypes(): List<TipoDiscotecaDao>
+    suspend fun getAllMarkerTypes(): List<TipoDiscoteca>
 
     @Query("SELECT * FROM tiposdiscotecas WHERE id = :id")
-    fun getMarkTypeById(id: Int): TipoDiscotecaDao?
+    fun getMarkTypeById(id: Int): TipoDiscoteca?
 }
