6612be98b94152aecc9350e6a1b73970d1366d1c
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-13 10:39:07 +0000

    MainApp.kt:Creamos MainApp

---
+++ app/src/main/java/com/example/finalprojectprogramacion/view/MainApp.kt
@@ -0,0 +1,6 @@
+package com.example.finalprojectprogramacion
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
+import org.osmdroid.tileprovider.tilesource.XYTileSource
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.util.MapTileIndex
+
