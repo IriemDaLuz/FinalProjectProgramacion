65611700c6f1cccd588e34c6e7d2b8dab994a38d
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-13 10:39:32 +0000

    MainApp.kt:Añadimos propiedades del mapa

--- app/src/main/java/com/example/finalprojectprogramacion/view/MainApp.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/view/MainApp.kt
@@ -4,3 +4,19 @@ import org.osmdroid.tileprovider.tilesource.XYTileSource
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.util.MapTileIndex
 
+
+val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
+    "Google-Sat",
+    0, 19, 256, ".png", arrayOf(
+        "https://mt0.google.com",
+        "https://mt1.google.com",
+        "https://mt2.google.com",
+        "https://mt3.google.com",
+    )
+) {
+    override fun getTileURLString(pTileIndex: Long): String {
+        return baseUrl + "/vt/lyrs=s&x=" + MapTileIndex.getX(pTileIndex) + "&y=" + MapTileIndex.getY(
+            pTileIndex
+        ) + "&z=" + MapTileIndex.getZoom(pTileIndex)
+    }
+}
