c96aae6e741c00056be308c80aad831122893950
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-13 10:44:17 +0000

    Creamos ReservaDao.kt

--- app/src/main/java/com/example/finalprojectprogramacion/data/ReservaDao.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/data/ReservaDao.kt
@@ -1,3 +1,29 @@
 package com.example.finalprojectprogramacion
-class ReservaDao {
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Delete
+
+@Dao
+interface ReservaDao {
+
+    @Insert
+    suspend fun insertReserva(reserva: Reserva): Long
+
+    @Update
+    suspend fun updateReserva(reserva: Reserva)
+
+    @Delete
+    suspend fun deleteReserva(reserva: Reserva)
+
+    @Query("SELECT * FROM reservas")
+    suspend fun getAllReservas(): List<Reserva>
+
+    @Query("SELECT * FROM reservas WHERE id = :id")
+    suspend fun getAllReservasById(id: Int): Reserva?
+
+    @Query("SELECT * FROM reservas WHERE idDiscoteca = :idDiscoteca")
+    suspend fun getAllReservasByDiscotecaId(idDiscoteca: Int): List<Reserva>
 }
