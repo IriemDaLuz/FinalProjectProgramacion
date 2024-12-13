9b04615bef1c38a8ccae8b7c31a89ae6d099c50a
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-13 11:26:00 +0000

    ViewModelApp.kt: Importamos Librerias

--- app/src/main/java/com/example/finalprojectprogramacion/viewmodel/ViewModelApp.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/viewmodel/ViewModelApp.kt
@@ -1,3 +1,9 @@
 package com.example.finalprojectprogramacion
 
-
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
