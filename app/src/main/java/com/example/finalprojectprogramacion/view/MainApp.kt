b109bd3d285b3ef10a7797127ad91cfd44daa65a
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-13 10:40:14 +0000

    MainApp.kt:Creamos composable del mapa

--- app/src/main/java/com/example/finalprojectprogramacion/view/MainApp.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/view/MainApp.kt
@@ -1,4 +1,43 @@
 package com.example.finalprojectprogramacion
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.utsman.osmandcompose.DefaultMapProperties
+import com.utsman.osmandcompose.Marker
+import com.utsman.osmandcompose.OpenStreetMap
+import com.utsman.osmandcompose.rememberCameraState
+import com.utsman.osmandcompose.rememberMarkerState
 import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
 import org.osmdroid.tileprovider.tilesource.XYTileSource
 import org.osmdroid.util.GeoPoint
@@ -20,3 +59,16 @@ val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
         ) + "&z=" + MapTileIndex.getZoom(pTileIndex)
     }
 }
+@Composable
+fun MainApp(ViewModelApp: ViewModelApp) {
+    val discotecas by ViewModelApp.discotecas.collectAsState()
+    val tiposdiscotecas by ViewModelApp.tiposdiscotecas.collectAsState()
+
+    val cameraState = rememberCameraState {
+        geoPoint = GeoPoint(28.964967918770267, -13.553658852564096)
+        zoom = 13.5
+    }
+
+    var mapProperties by remember { mutableStateOf(DefaultMapProperties) }
+
+    }
