c90f2d2969f4f12ef45b68730ddda50878ce17ff
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-13 11:27:37 +0000

    ViewModelApp.kt: Creamos variables iniciales

--- app/src/main/java/com/example/finalprojectprogramacion/viewmodel/ViewModelApp.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/viewmodel/ViewModelApp.kt
@@ -9,4 +9,12 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class ViewModelApp(
-){}
+    private val discotecaDao: DiscotecaDao,
+    private val tipoDiscotecaDao: TipoDiscotecaDao,
+    val reservaDao: ReservaDao
+) : ViewModel() {
+   private val _discotecas = MutableStateFlow<List<Discoteca>>(emptyList())
+    val discotecas: StateFlow<List<Discoteca>> = _discotecas
+   private val _tiposdiscotecas = MutableStateFlow<Map<Int, String>>(emptyMap())
+    val tiposdiscotecas: StateFlow<Map<Int, String>> = _tiposdiscotecas
+}
