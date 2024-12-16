e874c86974e521a5636827c66eb775a5637810d5
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-16 13:58:38 +0000

    DiscScreen:Creamos diferentes archivos para organizar todo mejor

---
+++ app/src/main/java/com/example/finalprojectprogramacion/view/DiscScreen.kt
@@ -0,0 +1,63 @@
+package com.example.finalprojectprogramacion
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.finalprojectprogramacion.ViewModelApp
+
+
+@Composable
+fun DiscScreen( viewModelApp: ViewModelApp, navController: NavController){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Informacion de Discotecas", style = MaterialTheme.typography.headlineMedium)
+        Spacer(modifier = Modifier.height(32.dp))
+
+        //Espacio para Tarjetas de discotecas
+
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { navController.navigate("list") }) {
+                Icon(imageVector = Icons.Filled.Info, contentDescription = "Lista de Discotecas")
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+
+            Button(onClick = { navController.navigate("home") }) {
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "Mis Reservas")
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+
+            Button(onClick = { navController.navigate("map") }) {
+                Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "Ver Mapa")
+                Spacer(modifier = Modifier.width(8.dp))
+            }
+        }
+    }
+}
+
