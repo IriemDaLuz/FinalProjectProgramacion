e716d7494739e0a8b1f08466b497b4fda77e3467
unai5665 <unaiperez5665@gmail.com>
2024-12-17 19:40:44 +0000

    navegacion para la nueva pantalla

--- app/src/main/java/com/example/finalprojectprogramacion/view/MainApp.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/view/MainApp.kt
@@ -1,15 +1,15 @@
 package com.example.finalprojectprogramacion.view
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-
+import androidx.navigation.navArgument
 import com.example.finalprojectprogramacion.viewmodel.ViewModelApp
 import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
 import org.osmdroid.tileprovider.tilesource.XYTileSource
 import org.osmdroid.util.MapTileIndex
-
 
 val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
     "Google-Sat",
@@ -26,13 +26,27 @@ val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
         ) + "&z=" + MapTileIndex.getZoom(pTileIndex)
     }
 }
-// Logica para navegar entre pantallas
+
 @Composable
 fun MainApp(viewModelApp: ViewModelApp) {
     val navController = rememberNavController()
-    NavHost(navController, startDestination = "home") {
-        composable("home") { HomeScreen(navController) }
+    NavHost(navController = navController, startDestination = "home") {
+        // Pantalla de inicio
+        composable("home") { HomeScreen(navController, viewModelApp) }
+
+        // Pantalla del mapa
         composable("map") { MapScreen(viewModelApp, navController) }
+
+        // Pantalla de discotecas
         composable("discs") { DiscScreen(viewModelApp, navController) }
+
+        // Nueva pantalla de edición de reserva, con el ID de la discoteca como argumento
+        composable(
+            "editReserva/{discotecaId}",
+            arguments = listOf(navArgument("discotecaId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val discotecaId = backStackEntry.arguments?.getInt("discotecaId") ?: return@composable
+            EditReservaScreen(viewModelApp = viewModelApp, navController = navController, discotecaId = discotecaId)
+        }
     }
 }
