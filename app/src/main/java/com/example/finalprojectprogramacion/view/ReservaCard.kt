56dcddb5e850936a8f49268fe495b24ce60de5ad
unai5665 <unaiperez5665@gmail.com>
2024-12-17 19:42:47 +0000

    ReservaCard.kt: Agregamos estilos

--- app/src/main/java/com/example/finalprojectprogramacion/view/ReservaCard.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/view/ReservaCard.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
@@ -80,34 +81,46 @@ fun ReservaCard(
                     .padding(top = 8.dp),
                 horizontalArrangement = Arrangement.End
             ) {
+                // Botón para expandir/cerrar la descripción
+                Button(
+                    onClick = { expanded = !expanded },
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .align(alignment = Alignment.CenterVertically),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF0056FF), // Color de fondo del botón
+                        contentColor = Color.White
+                    )
+                ){
+                    Text(if (expanded) "Cerrar Descripción" else "Mostrar Descripción")
+                }
                 // Fila con botones de editar y eliminar
-                IconButton(
-                    onClick = { onUpdate(reserva) }
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Edit,
-                        contentDescription = "Editar",
-                        tint = Color(0xFF0056FF)
-                    )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    horizontalArrangement = Arrangement.End
+                ){
+                    IconButton(
+                        onClick = { onUpdate(reserva) },
+                        modifier = Modifier
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "Editar",
+                            tint = Color(0xFF0056FF)
+                        )
+                    }
+                    IconButton(
+                        onClick = { onDelete(reserva) }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = "Eliminar",
+                            tint = Color(0xFF0056FF)
+                        )
+                    }
                 }
-                IconButton(
-                    onClick = { onDelete(reserva) }
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Delete,
-                        contentDescription = "Eliminar",
-                        tint = Color(0xFF0056FF)
-                    )
-                }
-            }
-            // Botón para expandir/cerrar la descripción
-            Button(
-                onClick = { expanded = !expanded },
-                modifier = Modifier
-                    .padding(top = 8.dp)
-                    .align(Alignment.CenterHorizontally)
-            ) {
-                Text(if (expanded) "Cerrar Descripción" else "Mostrar Descripción")
             }
         }
     }
