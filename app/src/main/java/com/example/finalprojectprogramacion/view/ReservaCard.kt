11100ac05a7413f673100708bf36ec1222a36b85
unai5665 <unaiperez5665@gmail.com>
2024-12-17 17:55:11 +0000

    ReservaCard.kt: Creamos funcion ReservaCard

---
+++ app/src/main/java/com/example/finalprojectprogramacion/view/ReservaCard.kt
@@ -0,0 +1,33 @@
+package com.example.finalprojectprogrtamacion.view
+
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.finalprojectprogramacion.data.Discoteca
+import com.example.finalprojectprogramacion.data.Reserva
+
+@Composable
+fun ReservaCard(
+){
+
+}
