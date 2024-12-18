8fc775f6489153222b57156952b0c0bdfd9a11d4
IriemDaLuz <yenduuca2005@gmail.com>
2024-12-18 08:27:17 +0000

    Pequeñas modificaciones en viewmodel

--- app/src/main/java/com/example/finalprojectprogramacion/viewmodel/ViewModelApp.kt
+++ app/src/main/java/com/example/finalprojectprogramacion/viewmodel/ViewModelApp.kt
@@ -9,7 +9,6 @@ import com.example.finalprojectprogramacion.data.Reserva
 import com.example.finalprojectprogramacion.data.ReservaDao
 import com.example.finalprojectprogramacion.data.TipoDiscoteca
 import com.example.finalprojectprogramacion.data.TipoDiscotecaDao
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,118 +17,54 @@ import kotlinx.coroutines.launch
 class ViewModelApp(
     private val discotecaDao: DiscotecaDao,
     private val tipoDiscotecaDao: TipoDiscotecaDao,
-    val reservaDao: ReservaDao
+    private val reservaDao: ReservaDao
 ) : ViewModel() {
 
-    // Estado que guarda la lista de lugares. Usamos un MutableStateFlow para poder modificarlo.
+    // Estados observables
     private val _discotecas = MutableStateFlow<List<Discoteca>>(emptyList())
     val discotecas: StateFlow<List<Discoteca>> = _discotecas
 
-    // Estado que guarda los tipos de lugares. Usamos un MutableStateFlow para poder modificarlo.
     private val _tiposdiscotecas = MutableStateFlow<Map<Int, String>>(emptyMap())
     val tiposdiscotecas: StateFlow<Map<Int, String>> = _tiposdiscotecas
 
-    // Estado que guarda las reservas. Usamos un MutableStateFlow para poder modificarlo.
     private val _reservas = MutableStateFlow<List<Reserva>>(emptyList())
     val reservas: StateFlow<List<Reserva>> = _reservas
 
-    // Inicialización: se cargan los datos de lugares y tipos de lugares cuando se crea el ViewModel
     init {
-        cargarMarcadores() // Cargar lugares
-        cargarTiposMarcadores() // Cargar tipos de lugares
-        loadReservas()
+        //insertarTiposDiscotecas()
+        //insertarDiscotecas()
+        cargarDiscotecas()
+        cargarTiposDiscotecas()
+        cargarReservas()
     }
 
-    // Función para cargar los lugares desde la base de datos
-    private fun cargarMarcadores() {
+    private fun cargarDiscotecas() {
         viewModelScope.launch(Dispatchers.IO) {
-            val discotecaList = discotecaDao.getAllMarkers()
-            _discotecas.value =discotecaList
-        }
-    }
-    private fun loadReservas() {
-        viewModelScope.launch {
-            val reservasList = reservaDao.getAllReservas()
-            _reservas.value = reservasList
-        }
-    }
-
-    // Función para cargar los tipos de lugares desde la base de datos
-    private fun cargarTiposMarcadores() {
-        viewModelScope.launch(Dispatchers.IO) {
-            val types = tipoDiscotecaDao.getAllMarkerTypes()
-            _tiposdiscotecas.value = types.associate { it.id to it.name }
-        }
-    }
-    private fun insertarDiscotecas() {
-        // Lista de discotecas con los detalles de cada una
-        val discotecas = listOf(
-            Discoteca(
-                name ="Changó Lanzarote",
-                latitude = "28.96192",
-                longitude = "-13.55520",
-                IdTipoDiscoteca = 2,
-                horario = "22:00 - 04:00",
-                capacidad = "50 personas",
-                telefono = "+34 928 111 222"
-            ),Discoteca(
-                name = "Karma",
-                latitude = "28.96226",
-                longitude = "-13.53921",
-                IdTipoDiscoteca = 3,
-                horario = "22:00 - 06:00",
-                capacidad = "90 personas"
-            ),
-            Discoteca(
-                name = "Manhattan Cocktail Bar",
-                latitude = "28.95809",
-                longitude = "-13.55349",
-                IdTipoDiscoteca = 3,
-                description = "Lounge y discoteca con un ambiente cosmopolita.",
-                horario = "11:00 - 03:00",
-                capacidad = "100 personas",
-                telefono = "+34 609010125"
-            ),
-            Discoteca(
-                name = "Four Seasons Rock Cafè",
-                latitude = "28.99682",
-                longitude = "-13.49163",
-                IdTipoDiscoteca = 1,
-                description = "Un pub con música variada y excelente ambiente nocturno.",
-                horario = "23:00 - 06:00",
-                capacidad = "120 personas",
-                telefono = "+34 828 91 01 07"
-            ),
-            Discoteca(
-                name = "Rockola Matagorda",
-                latitude = "28.94028",
-                longitude = "-13.59956",
-                IdTipoDiscoteca = 1,
-                horario = "22:00 - 04:00",
-                capacidad = "50 personas"
-            ),
-        )
-
-        // Inserción en la base de datos usando una corrutina
-        CoroutineScope(Dispatchers.IO).launch {
-            discotecas.forEach { discoteca ->
-                discotecaDao.insert(discoteca)
+            try {
+                _discotecas.value = discotecaDao.getAllMarkers()
+            } catch (e: Exception) {
+                Log.e("ViewModelApp", "Error al cargar discotecas: ${e.message}")
             }
         }
     }
 
+    private fun cargarTiposDiscotecas() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val tipos = tipoDiscotecaDao.getAllMarkerTypes()
+                _tiposdiscotecas.value = tipos.associate { it.id to it.name }
+            } catch (e: Exception) {
+                Log.e("ViewModelApp", "Error al cargar tipos de discotecas: ${e.message}")
+            }
+        }
+    }
 
-    private fun insertarTiposDiscotecas() {
-        val tiposDiscotecas = listOf(
-            TipoDiscoteca(name = "Rock"),
-            TipoDiscoteca(name = "Latina"),
-            TipoDiscoteca(name = "Reggaeton"),
-            TipoDiscoteca(name = "Variada")
-        )
-
-        CoroutineScope(Dispatchers.IO).launch {
-            tiposDiscotecas.forEach { tipoDiscoteca ->
-                tipoDiscotecaDao.insertMarkerType(tipoDiscoteca)
+    private fun cargarReservas() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _reservas.value = reservaDao.getAllReservas()
+            } catch (e: Exception) {
+                Log.e("ViewModelApp", "Error al cargar reservas: ${e.message}")
             }
         }
     }
@@ -141,42 +76,42 @@ class ViewModelApp(
         idDiscoteca: Int,
         cantidadPersonas: Int,
         estado: String
-    ){ if (name.isBlank() ) return
+    ) {
+        if (name.isBlank() || idDiscoteca <= 0 || fechaReserva.isBlank() || fechaEvento.isBlank()) {
+            Log.e("ViewModelApp", "Parámetros inválidos para agregar reserva")
+            return
+        }
 
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             try {
-                val discoteca = reservaDao.getAllReservasByDiscotecaId(idDiscoteca)
-                if (discoteca != null) {
-                    val newReserva = Reserva(
-                        id = 0,
-                        fechaReserva = fechaReserva,
-                        fechaEvento = fechaEvento,
-                        idDiscoteca=idDiscoteca,
-                        cantidadPersonas=cantidadPersonas,
-                        estado = "confirmado"
-                    )
-                    reservaDao.insertReserva(newReserva)
-                    loadReservas() // Recargar reserva
-                } else {
-                    Log.e("REservaViewModel", "Discoteca no encontrado: $idDiscoteca")
-                }
+                val nuevaReserva = Reserva(
+                    id = 0, // El ID será generado automáticamente por la base de datos
+                    name = name,
+                    fechaReserva = fechaReserva,
+                    fechaEvento = fechaEvento,
+                    idDiscoteca = idDiscoteca,
+                    cantidadPersonas = cantidadPersonas,
+                    estado = estado
+                )
+                reservaDao.insertReserva(nuevaReserva)
+                cargarReservas() // Recargar reservas
             } catch (e: Exception) {
-                Log.e("REservaViewModel", "Error al añadir tarea: ${e.message}")
+                Log.e("ViewModelApp", "Error al agregar reserva: ${e.message}")
             }
         }
     }
-
 
     fun deleteReserva(reserva: Reserva) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             try {
                 reservaDao.deleteReserva(reserva)
-                loadReservas()
+                cargarReservas() // Recargar reservas
             } catch (e: Exception) {
-                Log.e("TaskViewModel", "Error al eliminar tarea: ${e.message}")
+                Log.e("ViewModelApp", "Error al eliminar reserva: ${e.message}")
             }
         }
     }
+
     fun updateReserva(
         reservaId: Int,
         fechaReserva: String,
@@ -184,24 +119,21 @@ class ViewModelApp(
         cantidadPersonas: Int,
         estado: String
     ) {
-        viewModelScope.launch {
-            try {
-                // Obtenemos la reserva por su ID usando el método getAllReservasById
-                val reserva = reservaDao.getAllReservasById(reservaId)
+        if (reservaId <= 0 || fechaReserva.isBlank() || fechaEvento.isBlank()) {
+            Log.e("ViewModelApp", "Parámetros inválidos para actualizar reserva")
+            return
+        }
 
-                // Si la reserva existe, la actualizamos
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val reserva = reservaDao.getAllReservasById(reservaId)
                 if (reserva != null) {
-                    // Actualizamos los valores de la reserva
                     reserva.fechaReserva = fechaReserva
                     reserva.fechaEvento = fechaEvento
                     reserva.cantidadPersonas = cantidadPersonas
                     reserva.estado = estado
-
-                    // Llamamos a updateReserva para actualizar la reserva en la base de datos
                     reservaDao.updateReserva(reserva)
-
-                    // Recargamos la lista de reservas para reflejar los cambios en la UI
-                    loadReservas()
+                    cargarReservas() // Recargar reservas
                 } else {
                     Log.e("ViewModelApp", "Reserva no encontrada: $reservaId")
                 }
@@ -211,5 +143,57 @@ class ViewModelApp(
         }
     }
 
+    fun insertarDiscotecas() {
+        val discotecas = listOf(
+            Discoteca(
+                name = "Changó Lanzarote",
+                latitude = "28.96192",
+                longitude = "-13.55520",
+                IdTipoDiscoteca = 2,
+                horario = "22:00 - 04:00",
+                capacidad = "50 personas",
+                telefono = "+34 928 111 222"
+            ),
+            Discoteca(
+                name = "Karma",
+                latitude = "28.96226",
+                longitude = "-13.53921",
+                IdTipoDiscoteca = 3,
+                horario = "22:00 - 06:00",
+                capacidad = "90 personas"
+            )
+            // Agrega más discotecas según sea necesario
+        )
 
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                discotecas.forEach { discoteca ->
+                    discotecaDao.insert(discoteca)
+                }
+                cargarDiscotecas()
+            } catch (e: Exception) {
+                Log.e("ViewModelApp", "Error al insertar discotecas: ${e.message}")
+            }
+        }
+    }
+
+    fun insertarTiposDiscotecas() {
+        val tiposDiscotecas = listOf(
+            TipoDiscoteca(name = "Rock"),
+            TipoDiscoteca(name = "Latina"),
+            TipoDiscoteca(name = "Reggaeton"),
+            TipoDiscoteca(name = "Variada")
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                tiposDiscotecas.forEach { tipo ->
+                    tipoDiscotecaDao.insertMarkerType(tipo)
+                }
+                cargarTiposDiscotecas()
+            } catch (e: Exception) {
+                Log.e("ViewModelApp", "Error al insertar tipos de discotecas: ${e.message}")
+            }
+        }
+    }
 }
